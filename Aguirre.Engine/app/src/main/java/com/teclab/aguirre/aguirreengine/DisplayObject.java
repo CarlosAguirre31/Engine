504844c97bb90a4691731d813afbc6282a1be51b
Alumno <Alumno>
2017-04-04 13:33:51 -0500

    plantilla displayObject

--- Aguirre.Engine/app/src/main/java/com/teclab/aguirre/aguirreengine/DisplayObject.java
+++ Aguirre.Engine/app/src/main/java/com/teclab/aguirre/aguirreengine/DisplayObject.java
@@ -1,7 +1,11 @@
 package com.teclab.aguirre.aguirreengine;
 
+import android.app.usage.UsageEvents;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.util.EventLog;
 import android.view.MotionEvent;
+import android.view.animation.Transformation;
 
 /**
  * Created by Heat on 01/11/2016.
@@ -11,6 +15,29 @@ public class DisplayObject {
     public int x,y;
     public DisplayObject parent;
     public Resources resources;
+    public int height;
+    public float opacity;
+    public int pivotX;
+    public int pivotY;
+    public int rotation;
+    public float scaleX;
+    public float scaleY;
+    public Transformation transformation;
+    public boolean visible;
+    public int width;
+
+    public void dispatchEvent(UsageEvents.Event e){}
+    public void dispatchEvent(String type){}
+    public void dispose(){}
+    /*
+    public Point globalToLocal(Point p){}
+    public Point localToGlobal(Point p){}
+    public abstract void onDraw(){}
+    */
+    public void primaryOperations(){}
+    public void touchDown(Point p, int index){}
+    public void touchMove(Point p, int index){}
+    public void touchUp(Point p, int index){}
 
     public DisplayObject(Resources resources){
         this.resources = resources;
@@ -20,9 +47,7 @@ public class DisplayObject {
         this.update();
     }
 
-    public void update(){
-
-    }
+    public void update(){}
 
     public void onTouchEvent(MotionEvent event) {
 
